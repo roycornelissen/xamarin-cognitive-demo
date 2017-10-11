3ff81550fb55824af9bbfe2be1c95f315d0f1fae
Geert van der Cruijsen <geert@vdcruijsen.net>
2017-10-11 20:27:02 +0200

    added demos for text analytics

--- CognitiveDemo/ViewModels/DemoViewModel.cs
+++ CognitiveDemo/ViewModels/DemoViewModel.cs
@@ -9,6 +9,14 @@ using Microsoft.ProjectOxford.Emotion.Contract;
 using System.Linq;
 
 using Xamarin.Forms;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using System.Text;
+using Microsoft.ProjectOxford.Text.Sentiment;
+using Microsoft.ProjectOxford.Text.Language;
+using Microsoft.ProjectOxford.Text.Core;
 
 namespace CognitiveDemo
 {
@@ -19,6 +27,7 @@ namespace CognitiveDemo
         public ObservableCollection<Item> Items { get; set; }
         public Command CheckFaceApi { get; set; }
         public Command CheckEmotionApi { get; set; }
+        public Command CheckTextAnalyticsApi { get; set; }
 
         MediaFile photo;
 
@@ -28,6 +37,111 @@ namespace CognitiveDemo
             get { return error; }
             set { SetProperty(ref error, value); }
         }
+
+
+        public DemoViewModel()
+        {
+            Title = "Cognitive Demos";
+            Items = new ObservableCollection<Item>();
+            CheckFaceApi = new Command(async () => await ExecuteCheckFaceApiCommand());
+            CheckEmotionApi = new Command(async () => await ExecuteCheckEmotionApiCommand());
+            CheckTextAnalyticsApi = new Command(async () => await ExecuteCheckTextAnalyticsApiCommand());
+
+            emotionClient = new EmotionServiceClient(ApiKeys.EmotionApiKey);
+
+        }
+
+        #region Face API
+
+        ImageSource facePicture;
+        public ImageSource FacePicture
+        {
+            get { return facePicture; }
+            set { SetProperty(ref facePicture, value); }
+        }
+
+        string faceResult;
+        public string FaceResult
+        {
+            get { return faceResult; }
+            set { SetProperty(ref faceResult, value); }
+        }
+
+        async Task ExecuteCheckFaceApiCommand()
+        {
+            if (IsBusy)
+                return;
+
+            IsBusy = true;
+
+            try
+            {
+                await CrossMedia.Current.Initialize();
+
+                // Take photo
+                if (CrossMedia.Current.IsCameraAvailable || CrossMedia.Current.IsTakePhotoSupported)
+                {
+                    photo = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
+                    {
+                        Name = "face.jpg",
+                        PhotoSize = PhotoSize.Small
+                    });
+
+                    if (photo != null)
+                    {
+                        FacePicture = ImageSource.FromStream(photo.GetStream);
+                    }
+                }
+                else
+                {
+                    error = "Camera unavailable.";
+                }
+
+                if (photo != null)
+                {
+                    using (var photoStream = photo.GetStream())
+                    {
+                        BinaryReader binaryReader = new BinaryReader(photoStream);
+                        var imagesBytes = binaryReader.ReadBytes((int)photoStream.Length);
+
+                        HttpClient client = new HttpClient();
+                        client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", ApiKeys.FaceApiKey);
+
+                        string uri = "https://westeurope.api.cognitive.microsoft.com/face/v1.0/detect";
+                        // Request parameters. A third optional parameter is "details".
+                        string requestParameters = "returnFaceId=true&returnFaceLandmarks=false&returnFaceAttributes=age,gender,headPose,smile,facialHair,glasses,hair,makeup,occlusion,accessories,blur,exposure,noise";
+
+
+                        HttpResponseMessage response;
+
+                        using (ByteArrayContent content = new ByteArrayContent(imagesBytes))
+                        {
+                            //sent byte array to cognitive services API
+                            content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                            response = await client.PostAsync(uri + "?" + requestParameters, content);
+
+                            //read response as string and deserialize
+                            string contentString = await response.Content.ReadAsStringAsync();
+                            FaceResult = JsonPrettyPrint(contentString);
+                        }
+                    }
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        #endregion
+
+        #region Emotion API
 
         ImageSource emotionPicture;
         public ImageSource EmotionPicture
@@ -41,39 +155,6 @@ namespace CognitiveDemo
         {
             get { return emotionResult; }
             set { SetProperty(ref emotionResult, value); }
-        }
-
-
-        public DemoViewModel()
-        {
-            Title = "Cognitive Demos";
-            Items = new ObservableCollection<Item>();
-            CheckFaceApi = new Command(async () => await ExecuteCheckFaceApiCommand());
-            CheckEmotionApi = new Command(async () => await ExecuteCheckEmotionApiCommand());
-
-            emotionClient = new EmotionServiceClient(ApiKeys.EmotionApiKey);
-
-        }
-
-        async Task ExecuteCheckFaceApiCommand()
-        {
-            if (IsBusy)
-                return;
-
-            IsBusy = true;
-
-            try
-            {
-                
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine(ex);
-            }
-            finally
-            {
-                IsBusy = false;
-            }
         }
 
         async Task ExecuteCheckEmotionApiCommand()
@@ -90,17 +171,10 @@ namespace CognitiveDemo
                 // Take photo
                 if (CrossMedia.Current.IsCameraAvailable || CrossMedia.Current.IsTakePhotoSupported)
                 {
-                    //photo = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
-                    //{
-                    //    Name = "emotion.jpg",
-                    //    PhotoSize = PhotoSize.Small
-                    //});
-                    photo = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+                    photo = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
                     {
-                        PhotoSize = PhotoSize.Custom,
-                        CustomPhotoSize = 40,
-                        Directory = "Sample",
-                        Name = "test.jpg"
+                        Name = "emotion.jpg",
+                        PhotoSize = PhotoSize.Small
                     });
 
                     if (photo != null)
@@ -110,7 +184,7 @@ namespace CognitiveDemo
                 }
                 else
                 {
-                    error= "Camera unavailable.";
+                    error = "Camera unavailable.";
                 }
 
                 if (photo != null)
@@ -123,12 +197,83 @@ namespace CognitiveDemo
                             var emotion = result.FirstOrDefault();
                             var highestScore = emotion.Scores.ToRankedList().FirstOrDefault();
                             EmotionResult = $"Top Emotion: {highestScore.Key} {highestScore.Value.ToString()}";
-                            
+
                         }
                         photo.Dispose();
                     }
                 }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
 
+        #endregion
+
+        #region ComputerVision
+        #endregion
+
+        #region customVision
+
+        #endregion
+
+        #region Text Analytics API
+
+        string textAnalyticsText;
+        public string TextAnalyticsText
+        {
+            get { return textAnalyticsText; }
+            set { SetProperty(ref textAnalyticsText, value); }
+        }
+
+        string textAnalyticsResult;
+        public string TextAnalyticsResult
+        {
+            get { return textAnalyticsResult; }
+            set { SetProperty(ref textAnalyticsResult, value); }
+        }
+
+        async Task ExecuteCheckTextAnalyticsApiCommand()
+        {
+            if (IsBusy)
+                return;
+
+            IsBusy = true;
+
+            try
+            {
+                LanguageClient languageClient = new LanguageClient(ApiKeys.TextAnalyticsApiKey);
+                languageClient.Url = "https://westeurope.api.cognitive.microsoft.com/text/analytics/v2.0/languages";
+                LanguageRequest lr = new LanguageRequest();
+                lr.Documents.Add(new Document()
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Text = TextAnalyticsText
+                });
+                var lrResult = await languageClient.GetLanguagesAsync(lr);
+                var language = lrResult.Documents.First().DetectedLanguages.First().Name;
+                var languageCode = lrResult.Documents.First().DetectedLanguages.First().Iso639Name;
+
+
+                SentimentClient textClient = new SentimentClient(ApiKeys.TextAnalyticsApiKey);
+                textClient.Url = "https://westeurope.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment";
+
+                SentimentRequest sr = new SentimentRequest();
+
+                sr.Documents.Add(new SentimentDocument()
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Text = TextAnalyticsText,
+                    Language = languageCode
+                });
+
+                var result = await textClient.GetSentimentAsync(sr);
+                TextAnalyticsResult = $"Language: {language}, Sentiment Score: {result.Documents.First().Score}"; 
 
             }
             catch (Exception ex)
@@ -140,5 +285,77 @@ namespace CognitiveDemo
                 IsBusy = false;
             }
         }
+
+        #endregion
+
+
+
+
+        #region helpers
+
+        static string JsonPrettyPrint(string json)
+        {
+            if (string.IsNullOrEmpty(json))
+                return string.Empty;
+
+            json = json.Replace(Environment.NewLine, "").Replace("\t", "");
+
+            StringBuilder sb = new StringBuilder();
+            bool quote = false;
+            bool ignore = false;
+            int offset = 0;
+            int indentLength = 3;
+
+            foreach (char ch in json)
+            {
+                switch (ch)
+                {
+                    case '"':
+                        if (!ignore) quote = !quote;
+                        break;
+                    case '\'':
+                        if (quote) ignore = !ignore;
+                        break;
+                }
+
+                if (quote)
+                    sb.Append(ch);
+                else
+                {
+                    switch (ch)
+                    {
+                        case '{':
+                        case '[':
+                            sb.Append(ch);
+                            sb.Append(Environment.NewLine);
+                            sb.Append(new string(' ', ++offset * indentLength));
+                            break;
+                        case '}':
+                        case ']':
+                            sb.Append(Environment.NewLine);
+                            sb.Append(new string(' ', --offset * indentLength));
+                            sb.Append(ch);
+                            break;
+                        case ',':
+                            sb.Append(ch);
+                            sb.Append(Environment.NewLine);
+                            sb.Append(new string(' ', offset * indentLength));
+                            break;
+                        case ':':
+                            sb.Append(ch);
+                            sb.Append(' ');
+                            break;
+                        default:
+                            if (ch != ' ') sb.Append(ch);
+                            break;
+                    }
+                }
+            }
+
+            return sb.ToString().Trim();
+        }
+
+        #endregion
+
     }
 }
